d375f8c34f6314dc012370ffb04d1985e78c65ff
Ilya Lunin <lunia56@mail.ru>
2023-03-24 18:50:29 +0300

    Skill Block 2

--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -10,69 +10,15 @@ import {point1} from "../main/Main";
 
 
 function Skills() {
-    const [isVisible1, setIsVisible1] = useState(false);
-    const [isVisible2, setIsVisible2] = useState(false);
-    const [isVisible3, setIsVisible3] = useState(false);
-
-
-
-
-
-
 
     return (
         <div className={s.skillsBlock}>
             <div className={styleContainer.container + " " + s.skillsContainer}>
                 <Title title={'Skills'}/>
                 <div className={s.skills}>
-                    {/*<div  class="block" className={s.skillSection}>*/}
-                    {/*    Hello World*/}
-                    {/*</div>*/}
-                    {/*<Skill title={"JS"}*/}
-                    {/*       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt dicta harum reprehenderit sequi, vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt dicta harum reprehenderit sequi, vitae."}/>*/}
-                    {/*<Skill title={"TypeScript"}*/}
-                    {/*       description={"Lorem ipsum dolor sit amet, consectetur adipisicing "}/>*/}
-                    {/*<Skill title={"React"}*/}
-                    {/*       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt di"}/>*/}
-
-
-                    <div>
-                        <Waypoint onEnter={() => setIsVisible1(true)}
-                                  fireOnRapidScroll={false}
-                                  bottomOffset='20%'
-                                  // topOffset="10%"
-                        />
-                        {isVisible1 && (
-                            <div className={s.block}>
-                                <Skill title={"JS"}
-                                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt dicta harum reprehenderit sequi, vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt dicta harum reprehenderit sequi, vitae."}/>
-                                <Waypoint onEnter={() => setIsVisible2(true)}
-                                          fireOnRapidScroll={false}
-                                          bottomOffset='20%'
-                                          // topOffset="10%"
-                                />
-                            </div>
-                        )}
-                        {isVisible2 && (
-                            <div className={s.block}>
-                                <Skill title={"TypeScript"}
-                                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing "}/>
-                                <Waypoint onEnter={() => setIsVisible3(true)}
-                                          fireOnRapidScroll={false}
-                                          bottomOffset='20%'
-                                          // topOffset="10%"
-                                />
-                            </div>
-                        )}
-                        {isVisible3 && (
-                            <div className={s.block}>
-                                <Skill title={"React"}
-                                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab deserunt di"}/>
-                            </div>
-                        )}
-                    </div>
-
-
+                    <WaypointContainer title1={'JavaScript'} title1={'TypeScript'} title1={'React'} description1={'d1'}description2={'d1'} description3={'d3'}/>
+                    <WaypointContainer title1={'Redux'} title1={'MaterialIU'} title1={'React'} description1={'d1'}description2={'d1'} description3={'d3'}/>
+                    <WaypointContainer title1={'AXIOS'} title1={'TypeScript'} title1={'React'} description1={'d1'}description2={'d1'} description3={'d3'}/>
                 </div>
             </div>
         </div>
@@ -82,4 +28,70 @@ function Skills() {
 export default Skills;
 
 
+
+ const WaypointContainer = ({title1,title2,title3,description1,description2,description3}) => {
+    const [isVisible1, setIsVisible1] = useState(false);
+    const [isVisible2, setIsVisible2] = useState(false);
+    const [isVisible3, setIsVisible3] = useState(false);
+
+
+
+    const onEnterHandler = (setIsVisible) => {
+        setTimeout(() => {
+            setIsVisible(true)
+        }, 500);
+    };
+
+    function handlePositionChange(block, props) {
+
+        if (block === 'isVisible3' && props.currentPosition === 'above') {
+            setIsVisible1(false);
+            setIsVisible2(false);
+            setIsVisible3(false);
+        }
+        if (block === 'isVisible1' && props.currentPosition === 'below') {
+            setIsVisible1(false);
+            setIsVisible2(false);
+            setIsVisible3(false);
+        }
+
+    }
+
+    return (
+        <div>
+            <Waypoint onEnter={() => onEnterHandler(setIsVisible1)}
+                      onPositionChange={(props) => handlePositionChange('isVisible1', props)}
+                      fireOnRapidScroll={false}
+                      bottomOffset='20%'
+            />
+            {isVisible1 && (
+                <div className={s.block}>
+                    <Skill title={title1}
+                           description={description1}/>
+                    <Waypoint onEnter={() => onEnterHandler(setIsVisible2)}
+                              fireOnRapidScroll={false}
+                              bottomOffset='20%'
+                    />
+                </div>
+            )}
+            {isVisible2 && (
+                <div className={s.block}>
+                    <Skill title={title2}
+                           description={description2}/>
+                    <Waypoint onEnter={() => onEnterHandler(setIsVisible3)}
+                              fireOnRapidScroll={false}
+                              onPositionChange={(props) => handlePositionChange('isVisible3', props)}
+                              bottomOffset='20%'
+                    />
+                </div>
+            )}
+            {isVisible3 && (
+                <div className={s.block}>
+                    <Skill title={title3}
+                           description={description3}/>
+                </div>
+            )}
+        </div>
+    );
+};
 
